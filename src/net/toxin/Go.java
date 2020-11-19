a4404fdf2a3bc14ead474ea6fd461913af9941fe
TheTOXIN <thetoksin@gmail.com>
2020-11-19 13:12:43 +0300

    controller

--- src/net/toxin/Go.java
+++ src/net/toxin/Go.java
@@ -2,28 +2,32 @@ package net.toxin;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.security.GeneralSecurityException;
+import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Go extends JFrame {
 
+    private static final String TITLE = "RoadToNoWhere";
+
+    private static final float FPS = 60.0f;
+    private static final float FOV = 0.75f;
+
     private static final int W = 1600;
     private static final int H = 768;
 
-    private final View panel = new View();
     private final Color[] colors = new Color[6];
     private final List<Line> lines = new ArrayList<>();
-    private final Generator generator = new Generator();
 
-    private final int speed = 200;
+    private final View view = new View();
+    private final Generator generator = new Generator();
+    private final Controller controller = new Controller();
+
+    private final int speed = 300;
     private final int roadCount = 300;
     private final int roadWidth = 2000;
     private final int segmentLen = 200;
     private final int positionCam = 1500;
-    private final float depthCam = 0.75f;
 
     private int player = 0;
     private int counter = 0;
@@ -34,19 +38,23 @@ public class Go extends JFrame {
 
         super.setDefaultCloseOperation(EXIT_ON_CLOSE);
         super.setFocusTraversalKeysEnabled(false);
-        super.addKeyListener(new Controller());
+        super.addKeyListener(this.controller);
         super.setLocationRelativeTo(null);
-        super.setTitle("RoadToNoWhere");
         super.setLocation(0, 0);
         super.setResizable(false);
         super.setFocusable(true);
         super.setVisible(true);
-        super.add(panel);
+        super.setTitle(TITLE);
+        super.add(this.view);
 
         super.pack();
     }
 
     private void init() {
+        Timer timer = new Timer((int) FPS, this.controller);
+        timer.setInitialDelay(0);
+        timer.start();
+
         this.colors[0] = Color.BLACK;
         this.colors[1] = new Color(16, 200, 16);
         this.colors[2] = new Color(0, 154, 0);
@@ -68,7 +76,7 @@ public class Go extends JFrame {
             double x = 0, dx = 0;
 
             for (int n = startPos; n < startPos + roadCount; n++) {
-                 if (n >= counter) new Line();
+                if (n >= counter) new Line();
 
                 Line curr = lines.get(n);
                 Line prev = lines.get(Math.max(0, n - 1));
@@ -107,15 +115,35 @@ public class Go extends JFrame {
         }
     }
 
-    private class Controller extends KeyAdapter {
+    private class Controller extends KeyAdapter implements ActionListener {
+
+        private boolean isUp, isLeft, isRight, isDown;
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (isUp) position += speed;
+            if (isLeft) player -= speed / 2;
+            if (isRight) player += speed / 2;
+            if (isDown && position >= speed) position -= speed;
+
+            view.repaint();
+        }
+
         @Override
         public void keyPressed(KeyEvent e) {
-            if (e.getKeyCode() == KeyEvent.VK_UP) position += speed;
-            if (e.getKeyCode() == KeyEvent.VK_LEFT) player -= speed;
-            if (e.getKeyCode() == KeyEvent.VK_RIGHT) player += speed;
-            if (e.getKeyCode() == KeyEvent.VK_DOWN && position >= speed) position -= speed;
+            processKey(e.getKeyCode(), true);
+        }
 
-            panel.repaint();
+        @Override
+        public void keyReleased(KeyEvent e) {
+            processKey(e.getKeyCode(), false);
+        }
+
+        private void processKey(int code, boolean press) {
+            if (code == KeyEvent.VK_W) isUp = press;
+            else if (code == KeyEvent.VK_A) isLeft = press;
+            else if (code == KeyEvent.VK_S) isDown = press;
+            else if (code == KeyEvent.VK_D) isRight = press;
         }
     }
 
@@ -130,7 +158,7 @@ public class Go extends JFrame {
         }
 
         public void compute(int camX, int camY, int camZ) {
-            this.scale = depthCam / (this.dZ - camZ);
+            this.scale = FOV / (this.dZ - camZ);
             this.sX = (1 + scale * (dX - camX)) * W / 2;
             this.sY = (1 - scale * (dY - camY)) * H / 2;
             this.sW = scale * roadWidth * W / 2;
